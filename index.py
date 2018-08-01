a68b4505b6cea8ae0be1642601d6e0372ffa9bd6
Pengdi Xia <xiapengdi@utexas.edu>
2018-08-01 00:39:15 -0500

    cloud connected

---
+++ index.py
@@ -0,0 +1,12 @@
+import os
+import urllib
+import jinja2
+import webapp2
+
+from google.appengine.api import users
+from storage import Stmessage
+from storage import createdPost
+
+
+
+# Login
--- storage.py
+++ storage.py
@@ -2,11 +2,17 @@ from google.appengine.ext import ndb
 # from google.appengine.api import taskqueue
 from google.appengine.ext.blobstore import blobstore
 
+import os
+import urllib
+import jinja2
 import webapp2
+
+from google.appengine.api import users
 
 #main model for each post
 class Stmessage (ndb.Model):
-    author = ndb.StructuredProperty(Author)
+    email = ndb.StringProperty(indexed=False)
+    # author = ndb.StructuredProperty(Author)
     create_time = ndb.DateTimeProperty(auto_now_add= True)
     # should be a list, allow one user post multiple pic in one post
     # image = ndb.StructuredProperty(Image)
@@ -17,19 +23,45 @@ class Stmessage (ndb.Model):
     # May need change to list, one post can have multiple tags
     # tag = ndb.StringProperty(index = False)
 
-class Author(ndb.Model):
+# class Author(ndb.Model):
     #anonymous number
     # nickname = ndb.StringProperty(required = True)
-    email = ndb.StringProperty(indexed=False)
+    # email = ndb.StringProperty(indexed=False)
 
-class Image(ndb.model):
-    img = ndb.BlobKeyProperty(required = True, indexed = False)
+# class Image(ndb.model):
+#     img = ndb.BlobKeyProperty(required = True, indexed = False)
 
-#return a key
-def getkey():
-    return ndb.Key("woodo", "default_key")
 
-def createPost(useremail, title, content, theme):
-    message = Stmessage(parent = getkey(), author = Author(useremail), title = title, content = content, theme = theme)
-    message.put()
-    return message
+# def createPost(useremail, title, content, theme):
+#     message = Stmessage(author = Author(useremail), title = title, content = content, theme = theme)
+#     message.put()
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class IndexPage(webapp2.RequestHandler):
+
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        template_values = {
+            'user': user,
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+app = webapp2.WSGIApplication([
+        ('/', IndexPage),
+], debug=True)
